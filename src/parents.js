808992d9b4fa93bd1a9e249dcc89bfd7d2f59bc7
Nikhil <nikhilganeshbhat@gmail.com>
2021-01-09 00:16:13 +0530

    removed unused imports

--- src/parents.js
+++ src/parents.js
@@ -6,7 +6,6 @@ import {
   Show,
   Create,
   Edit,
-  Filter,
   DisabledInput,
   SimpleShowLayout,
   SimpleForm,
@@ -14,86 +13,75 @@ import {
   TextInput,
   ShowButton,
   EditButton,
-  DeleteButton,
-  RichTextField,
-  SelectInput,
   DateInput,
   NumberField,
   DateField,
   EmailField,
-  NumberInput
+  NumberInput,
 } from "react-admin";
-import RichTextInput from "ra-input-rich-text";
 
- const ParentList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-           <TextField source="admission number" />
-            <TextField source="blood group" />
-            <NumberField source="class" />
-            <DateField source="dob" />
-            <EmailField source="email" />
-            <NumberField source="phone" />
-            <TextField source="profession" />
-            <TextField source="relationship" />
- 	    <TextField source="pname"/>
- 	    <EditButton />
-	    <ShowButton />
-        </Datagrid>
-    </List>
+const ParentList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="admission number" />
+      <TextField source="blood group" />
+      <NumberField source="class" />
+      <DateField source="dob" />
+      <EmailField source="email" />
+      <NumberField source="phone" />
+      <TextField source="profession" />
+      <TextField source="relationship" />
+      <TextField source="pname" />
+      <EditButton />
+      <ShowButton />
+    </Datagrid>
+  </List>
 );
-const ParentEdit = props => (
-    <Edit {...props}>
-        <SimpleForm>
-           
-            <DisabledInput source="id"/>
-            <TextInput source="blood group" />
-	     <TextInput source="admission number" />
-            <NumberInput source="class" />
-            <DateInput source="dob" />
-            <TextInput source="email" />
-            <NumberInput source="phone" />
-            <TextInput source="profession" />
-            <TextInput source="relationship" />
- 	    <TextInput source="pname"/>
-        </SimpleForm>
-    </Edit>
+const ParentEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <TextInput source="blood group" />
+      <TextInput source="admission number" />
+      <NumberInput source="class" />
+      <DateInput source="dob" />
+      <TextInput source="email" />
+      <NumberInput source="phone" />
+      <TextInput source="profession" />
+      <TextInput source="relationship" />
+      <TextInput source="pname" />
+    </SimpleForm>
+  </Edit>
 );
-const ParentCreate = props => (
-    <Create {...props}>
-         <SimpleForm>
-            <TextInput source="admission number" />
-            <TextInput source="blood group" />
-            <NumberInput source="class" />
-            <DateInput source="dob" />
-            <TextInput source="email" />
-            <NumberInput source="phone" />
-            <TextInput source="profession" />
-            <TextInput source="relationship" />
-	    <TextInput source="pname"/>
-        </SimpleForm>
-    </Create>
+const ParentCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="admission number" />
+      <TextInput source="blood group" />
+      <NumberInput source="class" />
+      <DateInput source="dob" />
+      <TextInput source="email" />
+      <NumberInput source="phone" />
+      <TextInput source="profession" />
+      <TextInput source="relationship" />
+      <TextInput source="pname" />
+    </SimpleForm>
+  </Create>
 );
-const ParentShow = props => (
-    <Show {...props}>
-        <SimpleShowLayout>
-      
-            <TextField source="admission number" />
-            <TextField source="blood group" />
-            <NumberField source="class" />
-            <DateField source="dob" />
-            <EmailField source="email" />
-            <NumberField source="phone" />
-            <TextField source="profession" />
-            <TextField source="relationship" />
-	    <TextField source="pname"/>
-        </SimpleShowLayout>
-    </Show>
+const ParentShow = (props) => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="admission number" />
+      <TextField source="blood group" />
+      <NumberField source="class" />
+      <DateField source="dob" />
+      <EmailField source="email" />
+      <NumberField source="phone" />
+      <TextField source="profession" />
+      <TextField source="relationship" />
+      <TextField source="pname" />
+    </SimpleShowLayout>
+  </Show>
 );
 
-export {
- ParentList,
- ParentEdit,
- ParentCreate,
- ParentShow,
-}
+export { ParentList, ParentEdit, ParentCreate, ParentShow };
--- src/students.js
+++ src/students.js
@@ -7,160 +7,168 @@ import {
   Show,
   Create,
   Edit,
-  Filter,
   Tab,
   DisabledInput,
-  SimpleShowLayout,
   TabbedShowLayout,
   SimpleForm,
   TextField,
   TextInput,
   ShowButton,
   EditButton,
-  DeleteButton,
-  RichTextField,
-  SelectInput,
   DateInput,
-  NumberField,
-  DateField,
-  EmailField,
   ReferenceManyField,
   SingleFieldList,
-  ChipField,
-  ReferenceInput,
-  AutocompleteInput
 } from "react-admin";
-import RichTextInput from "ra-input-rich-text";
 
- const StudentList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="admission number"/>
-	    <TextField source="name" />
-	    <TextField source="gender" />
-	    <TextField source="class" />
-	    <TextField source="department" />
-            <TextField source="academicYear" />
-            
-	    
-	    <ReferenceManyField label="Parent" reference="parents" target="id">
-                <SingleFieldList>
-                    <TextField source="pname" />
-                </SingleFieldList>
-            </ReferenceManyField>
-	     <EditButton />
-	    <ShowButton />
-        </Datagrid>
-    </List>
+const StudentList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="admission number" />
+      <TextField source="name" />
+      <TextField source="gender" />
+      <TextField source="class" />
+      <TextField source="department" />
+      <TextField source="academicYear" />
+
+      <ReferenceManyField label="Parent" reference="parents" target="id">
+        <SingleFieldList>
+          <TextField source="pname" />
+        </SingleFieldList>
+      </ReferenceManyField>
+      <EditButton />
+      <ShowButton />
+    </Datagrid>
+  </List>
 );
 
-const StudentEdit = props => (
-    <Edit {...props}>
-        <SimpleForm>
-            <DisabledInput source="id"/>
-            <TextInput source="academic year" />
-	        <TextInput source="admission number" />
-            <TextInput source="address" />
-            <TextInput source="city" />
-            <TextInput source="class" />
-            <TextInput source="country" />
-            <TextInput source="department" />
-            <DateInput source="dob" />
-            <TextInput source="doj" />
-            <TextInput source="gender" />
-            <TextInput source="location" />
-            <TextInput source="name" />
-            <TextInput source="phone" />
-            <TextInput source="pincode" />
-        </SimpleForm>
-    </Edit>
+const StudentEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <TextInput source="academic year" />
+      <TextInput source="admission number" />
+      <TextInput source="address" />
+      <TextInput source="city" />
+      <TextInput source="class" />
+      <TextInput source="country" />
+      <TextInput source="department" />
+      <DateInput source="dob" />
+      <TextInput source="doj" />
+      <TextInput source="gender" />
+      <TextInput source="location" />
+      <TextInput source="name" />
+      <TextInput source="phone" />
+      <TextInput source="pincode" />
+    </SimpleForm>
+  </Edit>
 );
-const StudentCreate = props => (
-    <Create {...props}>
-        <SimpleForm>
-	    <TextInput source="admission number" />
-            <TextInput source="Location" />
-            <DateInput source="academicYear" />
-            <TextInput source="address" />
-            <TextInput source="city" />
-            <TextInput source="class" />
-            <TextInput source="country" />
-            <DateInput source="dateOfJoining" />
-            <TextInput source="department" />
-            <TextInput source="dob" />
-            <TextInput source="gender" />
-            <TextInput source="name" />
-            <TextInput source="pincode" />
-            <TextInput source="profilePhoto" />
-	   
-        </SimpleForm>
-    </Create>
+const StudentCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="admission number" />
+      <TextInput source="Location" />
+      <DateInput source="academicYear" />
+      <TextInput source="address" />
+      <TextInput source="city" />
+      <TextInput source="class" />
+      <TextInput source="country" />
+      <DateInput source="dateOfJoining" />
+      <TextInput source="department" />
+      <TextInput source="dob" />
+      <TextInput source="gender" />
+      <TextInput source="name" />
+      <TextInput source="pincode" />
+      <TextInput source="profilePhoto" />
+    </SimpleForm>
+  </Create>
 );
-const StudentShow = props => (
-    <Show {...props}>
-       <TabbedShowLayout>
-	   <Tab label="Student Info">
-           
-	  <TextField source="admission number"/>
-	    <TextField source="name" />
-	    <TextField source="gender" />
-	    <TextField source="class" />
-	    <TextField source="department" />
-            <TextField source="Location" />
-            <TextField source="academicYear" />
-            <TextField source="address" />
-            <TextField source="city" />
-            <TextField source="country" />
-            <TextField source="dateOfJoining" />
-            <TextField source="dob" label="Date of Birth"/>
-            <TextField source="pincode" />
-            <TextField source="profilePhoto" />
-     	</Tab>
+const StudentShow = (props) => (
+  <Show {...props}>
+    <TabbedShowLayout>
+      <Tab label="Student Info">
+        <TextField source="admission number" />
+        <TextField source="name" />
+        <TextField source="gender" />
+        <TextField source="class" />
+        <TextField source="department" />
+        <TextField source="Location" />
+        <TextField source="academicYear" />
+        <TextField source="address" />
+        <TextField source="city" />
+        <TextField source="country" />
+        <TextField source="dateOfJoining" />
+        <TextField source="dob" label="Date of Birth" />
+        <TextField source="pincode" />
+        <TextField source="profilePhoto" />
+      </Tab>
 
-	<Tab label="Parent Info">  
-	<ReferenceManyField label="Parent" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="pname" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	<ReferenceManyField label="Profession" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="profession" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	<ReferenceManyField label="Email" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="email" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	<ReferenceManyField label="Phone" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="phone" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	<ReferenceManyField label="Relationship" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="relationship" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	<ReferenceManyField label="Date of Birth" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="dob" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	<ReferenceManyField label="Blood Group" reference="parents" target="admission number">
-                <SingleFieldList>
-                    <TextField source="blood group" />
-	        </SingleFieldList>
-	</ReferenceManyField>
-	</Tab>
-     </TabbedShowLayout>
-    </Show>
+      <Tab label="Parent Info">
+        <ReferenceManyField
+          label="Parent"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="pname" />
+          </SingleFieldList>
+        </ReferenceManyField>
+        <ReferenceManyField
+          label="Profession"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="profession" />
+          </SingleFieldList>
+        </ReferenceManyField>
+        <ReferenceManyField
+          label="Email"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="email" />
+          </SingleFieldList>
+        </ReferenceManyField>
+        <ReferenceManyField
+          label="Phone"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="phone" />
+          </SingleFieldList>
+        </ReferenceManyField>
+        <ReferenceManyField
+          label="Relationship"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="relationship" />
+          </SingleFieldList>
+        </ReferenceManyField>
+        <ReferenceManyField
+          label="Date of Birth"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="dob" />
+          </SingleFieldList>
+        </ReferenceManyField>
+        <ReferenceManyField
+          label="Blood Group"
+          reference="parents"
+          target="admission number"
+        >
+          <SingleFieldList>
+            <TextField source="blood group" />
+          </SingleFieldList>
+        </ReferenceManyField>
+      </Tab>
+    </TabbedShowLayout>
+  </Show>
 );
 
-export {
- StudentList,
- StudentEdit,
- StudentCreate,
- StudentShow,
-}
+export { StudentList, StudentEdit, StudentCreate, StudentShow };
